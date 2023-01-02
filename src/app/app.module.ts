798b4590e1f49e5037bc0f883b681b3b2238b7f4
yosefha <yosef.hagos@sva.de>
2023-01-02 15:13:44 +0100

    trying to fix CORS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { MusicModule } from './music/music.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
@@ -20,6 +21,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     MusicModule,
     SharedModule,
     AppRoutingModule,
--- src/app/shared/side-nav/side-nav.component.ts
+++ src/app/shared/side-nav/side-nav.component.ts
@@ -1,5 +1,8 @@
+import { ShowUser } from './../../core/api/v1/model/showUser';
 import { Component, Input, OnInit } from '@angular/core';
 import { Navitem } from "../models/navitems";
+
+import { UsersService } from "../../core/api/v1/api/users.service";
 
 @Component({
   selector: 'app-side-nav',
@@ -11,10 +14,14 @@ export class SideNavComponent implements OnInit {
   @Input()
   items?: Navitem[];
 
-  constructor() {  }
+  constructor(private users: UsersService) {  }
 
   ngOnInit(): void {
   }
 
-
+  public GetUserList() {
+    this.users.getAllUsersUsersGet().subscribe(data => {
+      console.log(data);
+    })
+  }
 }
