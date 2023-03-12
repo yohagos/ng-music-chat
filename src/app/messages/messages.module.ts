083299b265e6ccc3333e2229730858177f1709c4
yohagos <yosef.hagos@googlemail.com>
2023-03-12 16:27:37 +0100

    refactor: commit to stash before updates

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -5,6 +5,9 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSelectModule } from "@angular/material/select";
 
 import { MessagesComponent } from "./messages.component";
 import { WebsocketComponent } from './websocket/websocket.component';
@@ -19,9 +22,12 @@ import { WebsocketComponent } from './websocket/websocket.component';
     FormsModule,
     ReactiveFormsModule,
 
+    MatButtonModule,
     MatIconModule,
     MatInputModule,
-    MatListModule
+    MatListModule,
+    MatFormFieldModule,
+    MatSelectModule,
   ],
   providers: [
   ]
